5554995dea344d844bb5f54d19a53f91443546ce
Aksnv <aksenov.mailbox@gmail.com>
2019-11-04 23:58:38 +0300

    Функции

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 "use strict";
 
-let maxNum = +prompt('Enter the max number', '');
+// Вводим произвольное число с клавиатуры.
+// Программа выводит все простые числа от 2 до введенного числа
+
+/*let maxNum = +prompt('Enter the max number', '');
 let counter;
 
 for (let i = 2; i <= maxNum; i++) {
 	counter = 0;
 
-	for (let j = 2; j <= i; j++) {
-		if ((i % j === 0) && (i !== j)) {
+	for (let j = 2; j < i; j++) {
+		if (i % j === 0) {
 			counter++;
 		}
 	}
@@ -15,4 +18,37 @@ for (let i = 2; i <= maxNum; i++) {
 	if (!counter) {
 		console.log(i);
 	}
+}*/
+
+
+// Функция возвращает меньшее из 2-х чисел a и b
+
+/*function min(a, b) {
+	if (a < b) {
+		return a;
+	}
+	return b;
 }
+
+console.log(min(1, 1));*/
+
+
+// Вводим 2 произвольных целых числа больше 1 с клавиатуры (x и n).
+// Функция возвращает x в степени n
+
+/*let x = +prompt('Enter the x number', '');
+let n = +prompt('Enter the n number', '');
+
+function pow(x, n) {
+	let result = x;
+	for (let i = 1; i < n; i++) {
+		result *= x;
+	}
+	return result;
+}
+
+console.log(pow(x, n));*/
+
+
+// 
+
